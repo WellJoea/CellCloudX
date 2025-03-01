4ca4ceb5bae5fc68f82704e8edd0d55a11d0d721
WellJoea <48665450+WellJoea@users.noreply.github.com>
2025-03-01 18:55:16 +0800

    Update similarity_registration.py

--- cellcloudx/alignment/ccflib/similarity_registration.py
+++ cellcloudx/alignment/ccflib/similarity_registration.py
@@ -6,9 +6,9 @@ class SimilarityRegistration(EMRegistration):
         super().__init__(*args, **kwargs)
         self.isoscale = isoscale
         self.reg_core = 'isosimilarity' if isoscale else 'similarity'
-        self.R = self.xp.eye(self.D) 
-        self.t = self.xp.zeros(self.D)
-        self.s = self.xp.ones(self.D)
+        self.R = self.xp.eye(self.D).to(self.device)
+        self.t = self.xp.zeros(self.D).to(self.device)
+        self.s = self.xp.eye(self.D).to(self.device)
 
         self.normal_ = 'each' if self.normal_ is None else self.normal_
         self.tol =  1e-9 if self.tol is None else self.tol
@@ -30,44 +30,53 @@ class SimilarityRegistration(EMRegistration):
         Y_hat = self.Y - muY
 
         A = self.PX.transpose(1,0) @ Y_hat - self.xp.outer(muX, self.P1 @ Y_hat)
-        U, S, V = self.xp.linalg.svd(A, full_matrices=True)
-        C = self.xp.eye(self.D, dtype=self.floatx, device=self.device)
-        C[-1, -1] = self.xp.linalg.det(U @ V)
-        self.R = U @ C @ V
-
-        if self.xp.linalg.det(self.R) < 0:
-            U[:, -1] = -U[:, -1]
-            self.R = U @ C @ V
         trXPX = self.xp.sum(self.Pt1 * self.xp.sum(X_hat * X_hat, 1))
 
         if self.isoscale:
+            U, S, V = self.xp.linalg.svd(A, full_matrices=True)
+            C = self.xp.eye(self.D, dtype=self.floatx, device=self.device)
+            C[-1, -1] = self.xp.linalg.det(U @ V)
+            self.R = U @ C @ V
+            if self.xp.linalg.det(self.R) < 0:
+                U[:, -1] = -U[:, -1]
+                self.R = U @ C @ V
+
             trAR = self.xp.trace(A.transpose(1,0) @ self.R )
             trYPY = self.xp.sum(self.P1 * self.xp.sum(Y_hat * Y_hat, 1))
-            self.s = self.xp.ones(self.D)*(trAR/trYPY)
-            self.t = muX - self.s * (self.R @ muY)
-            self.TY = self.s * (self.Y @ self.R.transpose(1,0)) +  self.t #.T
-            self.sigma2 = (trXPX + self.s* self.s* trYPY - 2* self.s* trAR) / (self.Np * self.D)
+            s = trAR/trYPY
+            self.s = self.xp.eye(self.D).to(self.device) * s
+            self.t = muX - self.R @ self.s @ muY
+            self.TY = self.Y @ (self.R @ self.s).T +  self.t #.T
+            self.sigma2 = (trXPX + s* s* trYPY - 2* s* trAR) / (self.Np * self.D)
         else:
-            trYPY = self.xp.sum(self.P1 * self.xp.sum(Y_hat * Y_hat, 1))
             YPY = self.xp.sum( Y_hat * Y_hat *self.P1.unsqueeze(1), 0)
             YPY.masked_fill_(YPY == 0, self.eps)
+            # self.s = self.xp.diagonal(A.T @ self.R)/YPY
 
-            #TODO
-            self.s0 = self.xp.diagonal(C)*S/YPY
-            self.s = self.xp.diagonal(A.T @ self.R)/YPY
-            ############
+            max_iter = 100
+            error = True
+            iiter = 0
+            C = self.xp.eye(self.D, dtype=self.floatx, device=self.device)
+            while (error):
+                U, S, V = self.xp.linalg.svd(A @ self.s, full_matrices=True) #A @ s.T
+                C[-1, -1] = self.xp.linalg.det(U @ V)
+                self.R = U @ C @ V
+                if self.xp.linalg.det(self.R) < 0:
+                    U[:, -1] = -U[:, -1]
+                    self.R = U @ C @ V
+                s_pre = self.s
+                self.s = self.xp.diag(self.xp.diagonal(A.T @ self.R)/YPY)
+                iiter += 1
+                error = (self.xp.linalg.norm(self.s - s_pre) > 1e-9) and (iiter < max_iter)
 
-            self.t = muX -  (self.R * self.s) @ muY
-            self.TY = self.Y @ (self.s.unsqueeze(1) * self.R.transpose(1,0) ) +  self.t #.T
-    
-            trARS = self.xp.trace(A.transpose(1,0) @ (self.R * self.s))
+            self.t = muX -  (self.R @ self.s) @ muY
+            self.TY = self.Y @ (self.R @ self.s).transpose(1,0) +  self.t
+            trARS = self.xp.trace(self.R @ self.s @ A.transpose(1,0))
             trSSYPY = self.xp.sum((self.s **2) * YPY)
             self.sigma2 = (trXPX  - 2*trARS + trSSYPY) / (self.Np * self.D)
 
         if self.sigma2 < 0:
-            # self.sigma2 = self.xp.asarray(1/self.iteration)
             self.sigma2 = self.xp.abs(self.sigma2) * 10 #TODO
-
         qprev = self.q
         self.q = self.D * self.Np/2 * (1+self.xp.log(self.sigma2))
         self.diff = self.xp.abs(self.q - qprev)
@@ -82,15 +91,11 @@ class SimilarityRegistration(EMRegistration):
             self.tmat = self.xp.asarray(tmat, dtype=self.floatx)
             self.tmatinv = self.xp.linalg.inv(self.tmat)
     
-            # #TODO
+            # #TODO SVD or norm 
             # B = self.tmat[:-1, :-1]
-            # self.s = self.xp.linalg.det(B)**(1/(B.shape[0])) 
-            # self.R = B/self.s
-            # self.t = self.tmat[:-1, [-1]].T
-
         else:
             self.tmat = self.xp.eye(self.D+1, dtype=self.floatx, device=self.device)
-            self.tmat[:self.D, :self.D] = self.R * self.s.unsqueeze(0) #R @ S
+            self.tmat[:self.D, :self.D] = self.R @ self.s
             self.tmat[:self.D,  self.D] = self.t
             self.tmatinv = self.xp.linalg.inv(self.tmat)
             self.tform = self.tmat
@@ -111,9 +116,10 @@ class SimilarityRegistration(EMRegistration):
         self.transform_point()
 
         self.s *= self.Xs/self.Ys 
-        self.t = (self.t * self.Xs + self.Xm) - (self.R*self.s) @ self.Ym
-        self.tform = self.Xf @ self.tmat @ self.xp.linalg.inv(self.Yf)
-        self.tforminv = self.xp.linalg.inv(self.tform)
+        self.t = (self.t * self.Xs + self.Xm) - (self.R @ self.s) @ self.Ym
+        # self.tform = self.Xf @ self.tmat @ self.xp.linalg.inv(self.Yf)
+        # self.tforminv = self.xp.linalg.inv(self.tform)
+        self.update_transformer()
         self.TY = self.TY * self.Xs + self.Xm
         # TY = self.transform_point(self.Yr)
     
